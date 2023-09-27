3ff5a76774bfb69c227df73081ee8362d7349821
Harun-Azgd <harunshiashia10@gmail.com>
2023-09-27 13:03:44 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,15 +1,30 @@
 #include "main.h"
+
+int actual_sqrt_recursion(int n, int i);
 /**
-* _pow_recursion - returns the value of x rased to the power of y
-* @x: the value to multiply
-* @y: the times to multiply the value
-*Return: the value multiplied y times
+* _sqrt_recursion - returns the natural square root of a number
+* @n: number to calculate the square root
+* Return: the resulting square root
 */
-int _pow_recursion(int x, int y)
+int _sqrt_recursion(int n)
 {
-	if (y < 0)
+	if (n < 0)
 		return (-1);
-	if (y == 0)
-		return (1);
-	return (x * _pow_recursion(x, y - 1));
+	return (actual_sqrt_recursion(n, 0));
+}
+/**
+* actual_sqrt_recursion - recurses to find the natural
+* square of a root of a number
+* @n: number to calculate the square root
+* @i: iterator
+*
+* Return: the resulting square root
+*/
+int actual_sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (actual_sqrt_recursion(n, i + 1));
 }
